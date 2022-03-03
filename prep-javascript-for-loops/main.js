9ffd3baa77c0a054828d07c5f67fbcb6072d8a8c
Jae Song <easyjs26@gmail.com>
2022-03-03 22:18:43 +0000

    Completed prep-javascript-for-loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,14 @@
+// create your loops here.
+var i = 0;
+var z = '';
+
+for (; i < 10; i++) {
+  console.log(i);
+}
+for (i = 0; i < 20; i += 2) {
+  console.log(i);
+}
+for (i = 100; i > 0; i--) {
+  z = 'Time till explosion: ' + i + '!';
+  console.log(z);
+}
